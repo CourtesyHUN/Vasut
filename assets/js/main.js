523d7c9ada53a7ef1c056733e3451a77d4997ba2
CourtesyHUN <egyedirobika@gmail.com>
2023-02-20 14:29:13 +0100

    Megírtam működik (nagyrészt)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const allomasok = ["Budapest-Nyugati", "Zugló", "Kőbánya alsó", "Kőbánya-Kispest", "Pestszentlőrinc", "Szemeretelep","Ferihegy","Vecsés","Vecsés-Kertekalja", "Üllő", "Hosszúberek-Péteri", "Monor", "Monorierdő", "Pilis", "Albertirsa", "Ceglédbercel", "Ceglédbercel-Cserő", "Budai út", "Cegléd", "Abony", "Szolnok", "Szajol", "Törökszentmiklós", "Fegyvernek-Örményes", "Kisújszállás"]
-
+//#region ElötteUtána
 function ElotteUtana(megallo) 
 {
     var p = allomasok.indexOf(megallo);
@@ -30,3 +30,12 @@ function ElotteUtana(megallo)
     }
     return r;
 }
+//#endregion
+
+//#region Keres
+function Keres() 
+{
+    var anev = document.getElementById("allomasnev"); /* Állomás név */
+    document.getElementById("eredmeny").innerHTML = ElotteUtana(anev)
+}
+//#endregion
